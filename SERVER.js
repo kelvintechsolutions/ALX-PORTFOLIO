82a2973d480a40db6591a5cfd63dfc15577344f2
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-06-24 11:22:51 +0300

    UPDATED

---
+++ SERVER.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change the port number as needed
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const githubRoutes = require('./routes/githubRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/github', githubRoutes);
+app.use('/api/user', userRoutes);
+
+// Error handling middleware
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`GitHub Stats Analyzer app listening at http://localhost:${port}`);
+});
